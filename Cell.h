94ebe4c8388d5efc8c6c069c3b9fa6f66aa09ed6
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2015-11-13 22:04:48 +0900

    Added Cell Class

---
+++ Cell.h
@@ -0,0 +1,17 @@
+#ifndef ST_CELL_H_INCLUDED
+#define ST_CELL_H_INCLUDED 
+
+/*
+    Class's description
+*/
+class Cell
+{
+public:
+    double u0, u1;
+    double density;
+    double length;
+protected:
+};
+
+#endif // ST_CELL_H_INCLUDED
+

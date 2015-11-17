c59c1d94e034f4c47e967ff933178bee7fd425c7
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2015-11-17 09:51:29 +0900

    Renamed length to size

--- Cell.h
+++ Cell.h
@@ -9,18 +9,18 @@ class Cell
 public:
     double u0[2], u1[2];
     double density;
-    double length;
+    double size;
     Cell(){
         this->u0[0] = 0.0;
         this->u0[1] = 0.0;
         this->density = 1,0;
-        this->length = 1,0;
+        this->size = 1,0;
     }
-    Cell(double x, double y, double d, double l){
+    Cell(double x, double y, double d, double s){
         this->u0[0] = x;
         this->u0[1] = y;
         this->density = d;
-        this->length = l;
+        this->size = s;
     }
 protected:
 };

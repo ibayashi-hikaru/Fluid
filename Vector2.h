e629184011f06cf793d1a7f2c6e8eee7219d9003
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2015-11-17 23:15:48 +0900

    Added Vector2 operator

--- Vector2.h
+++ Vector2.h
@@ -10,6 +10,16 @@ public:
      Vector2() : x(0), y(0){}
      Vector2(TYPE val_x, TYPE val_y) : x(val_x), y(val_y){}
      TYPE x, y;
+     Vector2 operator+(const Vector2<TYPE>& v) const{
+         return Vector2<TYPE> (x + v.x, y + v.y);
+     }
+     Vector2 operator-(const Vector2<TYPE>& v) const{
+         
+         return Vector2<TYPE> (x - v.x, y - v.y);
+     }
+     Vector2 operator*(TYPE a) const{
+         return Vector2<TYPE> (x * a, y * a);
+     }
 protected:
 };
 

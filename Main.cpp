0ebca5bba60a080d6ae8ce78c06eb02158a4e55a
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2015-12-08 14:40:15 +0900

    Implemented export function

--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,30 @@
 #include "Grid.h"
 
 using namespace Eigen;
+void export_velocity_field(Grid field);
 int main(int argc, char** argv){
-    Grid field(512, 512);
-    for(int i=0; i<512; i++){
-        for(int j=0; j<512; j++){
+    Grid field(64, 64);
+    for(int i=0; i<field.height; i++){
+        for(int j=0; j<field.length; j++){
             field.cells[i][j].u0.x() = 1.0;
             field.cells[i][j].u0.y() = 0.0;
             field.cells[i][j].u1.x() = 1.0;
             field.cells[i][j].u1.y() = 0.0;
         } 
     }
-    std::cout << field.getVelocity(Vector2d{36.6, 105.4}) << std::endl;
+    export_velocity_field(field);
     return 0;
+}
+
+void export_velocity_field(Grid field){
+    std::cout << "set xrange [" << 0 << ":" << field.length <<"]" << std::endl;
+    std::cout << "set yrange [" << 0 << ":" << field.height <<"]" << std::endl;
+    int id = 0;
+    for(int i=0; i < field.length; i++){
+        for(int j=0; j < field.height; j++){
+            id++;   
+            std::cout << "set arrow " << id << " from " << i << "," << j << " to " << i + field.cells[i][j].u1.x() << "," <<  j + field.cells[i][j].u1.y() << std::endl;
+        } 
+    }
+    std::cout << "plot 0/1 notitle" << std::endl;
 }

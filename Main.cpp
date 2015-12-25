bd5f4d0022e5479f20e77588869ddf156e0f7fac
Hikaru Ibayashi <hikaru.ccm1023@gmail.com>
2015-12-25 17:34:17 +0900

    Updated main.cpp to long time version

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,7 @@ const double PI = M_PI;
 const double NU = 0.01;
 using namespace Eigen;
 int main(int argc, char** argv) {
-    Field field(2, 2);
+    Field field(64, 64);
     for(int i=0; i<field.height; i++) {
         for(int j=0; j<field.width; j++) {
             field.cells[i][j].u0.x() = 1.0;
@@ -19,17 +19,17 @@ int main(int argc, char** argv) {
             field.cells[i][j].u1.y() = 0.0;
         } 
     }
-    for(int i=0; i < field.width; i++) {
-        field.cells[i][field.height/2].force.y() = 4.0;
-    }
+    field.makeSquareForceSource();
     field.CALC_STEP = STEP0;
-    field.addForce(0.1);
-    field.addTransport(0.1);
-    field.FFT2d();
-    field.addDiffuse(0.1);
-    field.projectField();
-    field.invFFT2d();
-    field.swapVelocity();
+    for(int i = 0; i < 100; i++) {
+        field.addForce(0.1);
+        field.addTransport(0.1);
+        field.FFT2d();
+        field.addDiffuse(0.1);
+        field.projectField();
+        field.invFFT2d();
+        field.swapVelocity();
+    }
     InterfaceUtility::export_u0field_to_gnuplot(field);
     return 0;
 }

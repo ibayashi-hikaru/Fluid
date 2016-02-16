0b0029c3c0bdbd90834380c5bb15eeaa21408ae3
尉林暉 <hikaru.ccm1023@gmail.com>
2016-02-16 14:42:06 +0900

    preliminary version. tentatively compiler doesn't print any error

--- 3dMAC/Main.cpp
+++ 3dMAC/Main.cpp
@@ -16,16 +16,17 @@ void drawVelocity() {
 	glLineWidth(1.0f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(-1.0, -1.0, 0);
-    glScaled(2.0/field.GridNum(), 2.0/field.GridNum(), 1.0);
+    glTranslatef(-1.0, -1.0, -1.0);
+    glScaled(2.0/field.GridNum(), 2.0/field.GridNum(), 2.0/field.GridNum());
 	glBegin(GL_LINES);
-        double windowDx = 2.0 / field.GridNum();
         for(int i = 0; i < field.GridNum(); i++) {
             for(int j = 0; j < field.GridNum(); j++) {
-                Vector2d fieldPosition((i + 0.5) * 1.0, (j + 0.5) * 1.0);
-                Vector2d fieldVelocityPosition = fieldPosition + field.GetVelocity(fieldPosition);
-			    glVertex2d(fieldPosition.x(), fieldPosition.y());
-			    glVertex2d(fieldVelocityPosition.x(), fieldVelocityPosition.y());
+                for(int k = 0; k < field.GridNum(); k++) {
+                    Vector3d fieldPosition((i + 0.5) * field.Dx(), (j + 0.5) * field.Dx(), (k + 0.5) * field.Dx());
+                    Vector3d fieldVelocityPosition = fieldPosition + field.GetVelocity(fieldPosition);
+			        glVertex3d(fieldPosition.x(), fieldPosition.y(), fieldPosition.z());
+			        glVertex3d(fieldVelocityPosition.x(), fieldVelocityPosition.y(), fieldVelocityPosition.z());
+                }
             }
         }
 	glEnd ();
@@ -38,12 +39,14 @@ void drawPoints() {
     glPointSize(2.0f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(-1.0, -1.0, 0);
-    glScaled(2.0/field.GridNum(), 2.0/field.GridNum(), 1.0);
+    glTranslatef(-1.0, -1.0, -1.0);
+    glScaled(2.0/field.GridNum(), 2.0/field.GridNum(), 2.0/field.GridNum());
 	glBegin(GL_POINTS);
         for(int i = 0; i < field.GridNum(); i++) {
             for(int j = 0; j < field.GridNum(); j++) {
-			    glVertex2d(points.at(i).at(j).x(), points.at(i).at(j).y());
+                for(int k = 0; k < field.GridNum(); k++) {
+			        glVertex3d(points.at(i).at(j).at(k).x(), points.at(i).at(j).at(k).y(), points.at(i).at(j).at(k).z());
+                }
             }
         }
 	glEnd ();
@@ -52,30 +55,6 @@ void drawPoints() {
 }
 
 void drawMarble() {
-    auto forward = marbleEdge.begin();
-    auto backward = forward;
-	glColor3f(0.0f, 0.0f, 1.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef(-1.0, -1.0, 0);
-    glScaled(2.0/field.GridNum(), 2.0/field.GridNum(), 1.0);
-    glEnable(GL_COLOR_LOGIC_OP);
-    glLogicOp(GL_INVERT);
-    // 三角形ポリゴンを描く
-    glBegin(GL_TRIANGLES);
-    for(forward++; forward!=marbleEdge.end(); forward++) {
-        glVertex2d(0.0,0.0);
-        glVertex2d(backward->x(), backward->y());
-        glVertex2d(forward->x(), forward->y());
-        backward = forward;
-    }
-        glVertex2d(0.0,0.0);
-        glVertex2d(backward->x(), backward->y());
-        glVertex2d(marbleEdge.begin()->x(), marbleEdge.begin()->y());
-    glEnd();
-    glDisable(GL_COLOR_LOGIC_OP);
-    glPopMatrix();
-    glFlush();
 }
 
 void myDisplay(void) {
@@ -99,24 +78,17 @@ void myKeyboard(unsigned char key, int x, int y) {
 }
 
 void initPoints() {
-    points = vector< vector<Vector2d>>(field.GridNum(), vector<Vector2d>(field.GridNum()));
+    points = vector< vector< vector<Vector3d>>>(field.GridNum(), vector< vector<Vector3d>>(field.GridNum(), vector<Vector3d>(field.GridNum())));
     for(int i = 0; i < field.GridNum(); i++) {
         for(int j = 0; j < field.GridNum(); j++) {
-            points.at(i).at(j) = Vector2d((i + 0.5) * 1.0, (j + 0.5) * 1.0); 
+            for(int k = 0; k < field.GridNum(); k++) {
+                points.at(i).at(j).at(k) = Vector3d((i + 0.5) * field.Dx(), (j + 0.5) * field.Dx(), (k + 0.5) * field.Dx()); 
+            }
         } 
     }
 }
 
 void initMarble() {
-    marbleEdge.clear();
-    for(int i = 0; i < marbleCount; i++) {
-        marbleEdge.push_back(
-                Vector2d(
-                    field.GridNum() / 2.0 + (field.GridNum() * 0.25) * sin((2 * M_PI) * (i/(double)(marbleCount))),
-                    field.GridNum() / 2.0 + (field.GridNum() * 0.25) * cos((2 * M_PI) * (i/(double)(marbleCount)))
-                    )
-                ); 
-    }
 }
 
 void myInit() {
@@ -137,13 +109,6 @@ void updateDeltaTime() {
 }
 
 void updateForce() {
-    Vector2d lastFieldPosition = field.TransformDisplayToField(lastPosition, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
-    Vector2d currentFieldPosition = field.TransformDisplayToField(currentPosition, glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
-    Vector2d force = 1.0 * (currentFieldPosition - lastFieldPosition)/deltaTime; //速度に比例した力 10.0は比例定数
-    Vector2d position = currentFieldPosition;
-    field.SetForce(force, position);
-    lastPosition.x() = currentPosition.x();
-    lastPosition.y() = currentPosition.y();
 }
 
 void updateField(double timeStep) {
@@ -156,39 +121,14 @@ void updateField(double timeStep) {
 void updatePoints(double timeStep) {
     for(int i = 0; i < field.GridNum(); i++) {
         for(int j = 0; j < field.GridNum(); j++) {
-            points.at(i).at(j) += timeStep * field.GetVelocity(points.at(i).at(j)); 
-        } 
-    }
-}
-void resample() {
-    double min_ds = field.Dx()/10.0;
-    double max_ds = field.Dx();
-    auto forward = marbleEdge.begin();
-    auto backward = forward;
-    for( forward++; forward!=marbleEdge.end(); ) {
-        Vector2d &p0 = *backward;
-        Vector2d &p1 = *forward;
-        double d = (p0 - p1).norm();
-        if( d < min_ds ) { 
-        // 頂点間の距離が狭すぎる場合
-        // 2つの頂点を結合する
-            p0 = 0.5 * (p0 + p1);
-            forward = marbleEdge.erase(forward);
-        } else if( d > max_ds ) { // 頂点間の幅が広すぎる場合 // 間に新しい頂点を挿入する
-            Vector2d p = 0.5 * (p0 + p1);
-            forward = marbleEdge.insert(forward,p);
-        } else {
-            backward = forward; 
-            forward ++;
+            for(int k = 0; k < field.GridNum(); k++) {
+                points.at(i).at(j).at(k) += timeStep * field.GetVelocity(points.at(i).at(j).at(k)); 
+            }
         } 
     }
 }
 
 void updateMarble(double timeStep) {
-    for(auto itr = marbleEdge.begin(); itr != marbleEdge.end(); ++itr) {
-        *itr += timeStep * field.GetVelocity(*itr);
-    }
-    resample();
 }
 
 void myIdle(void) {
@@ -199,7 +139,6 @@ void myIdle(void) {
     }
     updateField(timeStep);
     updatePoints(timeStep);
-    updateMarble(timeStep);
     glutPostRedisplay();
     cout << "\rdeltaTime: " << deltaTime;
     fflush(stdout);

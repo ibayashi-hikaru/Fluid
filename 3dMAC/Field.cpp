9ce9d6c07dd1bf968855eec5b25e189ad2050782
尉林暉 <hikaru.ccm1023@gmail.com>
2016-02-18 12:29:02 +0900

    Fixed

--- 3dMAC/Field.cpp
+++ 3dMAC/Field.cpp
@@ -116,50 +116,69 @@ Field::GS_Project(double dt) {
 
 void
 Field::CG_Project(double dt) {
-    // VectorXd x(Nx*Ny), b(Nx*Ny);
-    // SparseMatrix<double> A(Nx*Ny, Nx*Ny);
-    // A.reserve(static_cast<int> (5*Nx*Ny));
-    // double invScale = (rho * dx * dx) / dt;
-    // for(int i = 0; i < Nx; i++) {
-    //     for(int j = 0; j < Ny; j++) {
-    //         vector<double> D = {1.0, 1.0, -1.0, -1.0}; 
-    //         vector<double> F = {static_cast<double>(i < Nx - 1),
-    //                             static_cast<double>(j < Ny - 1),
-    //                             static_cast<double>(i > 0),
-    //                             static_cast<double>(j > 0)};
-    //         vector<double> U = {ux[i + 1][j], uy[i][j + 1], ux[i][j], uy[i][j]};
-    //         double sum_R = 0.0;
-    //         for(int n = 0; n < 4; n++) {
-    //             sum_R += invScale * F[n] * D[n] * U[n]/dx;
-    //         }
-    //         b[i*Ny + j] = sum_R;
-    //         if(F.at(0)) A.insert(i*Ny + j, (i + 1)*Ny + (j + 0)) += 1.0;
-    //         if(F.at(1)) A.insert(i*Ny + j, (i + 0)*Ny + (j + 1)) += 1.0;
-    //         if(F.at(2)) A.insert(i*Ny + j, (i - 1)*Ny + (j + 0)) += 1.0;
-    //         if(F.at(3)) A.insert(i*Ny + j, (i + 0)*Ny + (j - 1)) += 1.0;
-    //         A.insert(i*Ny + j, (i + 0)*Ny + (j + 0)) -= 4.0;
-    //     } 
-    // }
-    // ConjugateGradient<SparseMatrix<double> > cg;
-    // cg.setTolerance(1.0e-1);
-    // cg.compute(A);
-    // x = cg.solve(b);
-    // for(int i = 0; i < Nx; i++) {
-    //     for(int j = 0; j < Ny; j++) {
-    //         p[i][j] = x[i*Ny + j];
-    //     } 
-    // }
+    VectorXd x(Nx*Ny*Nz), b(Nx*Ny*Nz);
+    SparseMatrix<double> A(Nx*Ny*Nz, Nx*Ny*Nz);
+    A.reserve(static_cast<int> (7*Nx*Ny*Nz));
+    double invScale = (rho * dx * dx) / dt;
+    for(int i = 0; i < Nx; i++) {
+        for(int j = 0; j < Ny; j++) {
+            for(int k = 0; k < Nz; k++) {
+                vector<double> D = {1.0, 1.0, 1.0, -1.0, -1.0, -1.0}; 
+                vector<double> F = {static_cast<double>(i < Nx - 1),
+                                    static_cast<double>(j < Ny - 1),
+                                    static_cast<double>(k < Nz - 1),
+                                    static_cast<double>(i > 0),
+                                    static_cast<double>(j > 0),
+                                    static_cast<double>(k > 0)};
+                vector<double> U = {ux[i + 1][j][k], uy[i][j + 1][k], uz[i][j][k + 1], ux[i][j][k], uy[i][j][k], uz[i][j][k]};
+                double sum_R = 0.0;
+                for(int n = 0; n < 6; n++) {
+                    sum_R += invScale * F[n] * D[n] * U[n]/dx;
+                }
+                b[i*(Ny*Nz) + j*Nz + k] = sum_R;
+                if(F.at(0)) A.insert(i*(Ny*Nz) + j*Nz + k, (i + 1)*(Ny*Nz) + (j + 0)*Nz + (k + 0)) += 1.0;
+                if(F.at(1)) A.insert(i*(Ny*Nz) + j*Nz + k, (i + 0)*(Ny*Nz) + (j + 1)*Nz + (k + 0)) += 1.0;
+                if(F.at(2)) A.insert(i*(Ny*Nz) + j*Nz + k, (i + 0)*(Ny*Nz) + (j + 0)*Nz + (k + 1)) += 1.0;
+                if(F.at(3)) A.insert(i*(Ny*Nz) + j*Nz + k, (i - 1)*(Ny*Nz) + (j + 0)*Nz + (k + 0)) += 1.0;
+                if(F.at(4)) A.insert(i*(Ny*Nz) + j*Nz + k, (i + 0)*(Ny*Nz) + (j - 1)*Nz + (k + 0)) += 1.0;
+                if(F.at(5)) A.insert(i*(Ny*Nz) + j*Nz + k, (i + 0)*(Ny*Nz) + (j + 0)*Nz + (k - 1)) += 1.0;
+                A.insert(i*(Ny*Nz) + j*Nz + k, i*(Ny*Nz) + j*Nz + k) -= 6.0;
+            }
+        } 
+    }
+    ConjugateGradient<SparseMatrix<double> > cg;
+    cg.setTolerance(1.0e-1);
+    cg.compute(A);
+    x = cg.solve(b);
+    for(int i = 0; i < Nx; i++) {
+        for(int j = 0; j < Ny; j++) {
+            for(int k = 0; k < Nz; k++) {
+                 p[i][j][k] = x[i*(Ny*Nz) + j*Nz + k];
+            }
+        } 
+    }
 
-    // for(int i = 1; i < Nx; i++) {
-    //     for(int j = 0; j < Ny; j++) {
-    //          ux.at(i).at(j) = ux.at(i).at(j) - (dt/rho) * ((p.at(i).at(j) - p.at(i-1).at(j))/dx);
-    //     } 
-    // }    
-    // for(int i = 0; i < Nx; i++) {
-    //     for(int j = 1; j < Ny; j++) {
-    //          uy.at(i).at(j) = uy.at(i).at(j) - (dt/rho) * ((p.at(i).at(j) - p.at(i).at(j-1))/dx);
-    //     } 
-    // }    
+    for(int i = 1; i < Nx; i++) {
+        for(int j = 0; j < Ny; j++) {
+            for(int k = 0; k < Nz; k++) {
+                 ux.at(i).at(j).at(k) = ux.at(i).at(j).at(k) - (dt/rho) * ((p.at(i).at(j).at(k) - p.at(i-1).at(j).at(k))/dx);
+            }
+        } 
+    }    
+    for(int i = 0; i < Nx; i++) {
+        for(int j = 1; j < Ny; j++) {
+            for(int k = 0; k < Nz; k++) {
+                uy.at(i).at(j).at(k) = uy.at(i).at(j).at(k) - (dt/rho) * ((p.at(i).at(j).at(k) - p.at(i).at(j-1).at(k))/dx);
+            }
+        } 
+    }    
+    for(int i = 0; i < Nx; i++) {
+        for(int j = 0; j < Ny; j++) {
+            for(int k = 1; k < Nz; k++) {
+                uz.at(i).at(j).at(k) = uz.at(i).at(j).at(k) - (dt/rho) * ((p.at(i).at(j).at(k) - p.at(i).at(j).at(k-1))/dx);
+            }
+        } 
+    }    
 }
 
 Vector3d

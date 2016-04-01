7698552285831da6bb9088ae4a1510aa8bfc4aa5
尉林暉 <hikaru.ccm1023@gmail.com>
2016-04-01 11:34:15 +0900

    2016年 4月 1日 金曜日 11時34分14秒 JST

--- 3dMAC/Field.cpp
+++ 3dMAC/Field.cpp
@@ -438,27 +438,27 @@ Field::Extrapolate() {
             }
         }
 
-        for(size_t i = 1; i < Nx; i++) {
-            for(size_t j = 0; j < Ny; j++) {
-                for(size_t k = 0; k < Nz; k++) {
-                    xSwap[i][j][k] = ux[i][j][k];
-                }
+    for(size_t i = 1; i < Nx; i++) {
+        for(size_t j = 0; j < Ny; j++) {
+            for(size_t k = 0; k < Nz; k++) {
+                ux[i][j][k] = xSwap[i][j][k];
             }
         }
-        for(size_t i = 0; i < Nx; i++) {
-            for(size_t j = 1; j < Ny; j++) {
-                for(size_t k = 0; k < Nz; k++) {
-                    ySwap[i][j][k] = uy[i][j][k];
-                }
+    }
+    for(size_t i = 0; i < Nx; i++) {
+        for(size_t j = 1; j < Ny; j++) {
+            for(size_t k = 0; k < Nz; k++) {
+                uy[i][j][k] = ySwap[i][j][k];
             }
         }
-        for(size_t i = 0; i < Nx; i++) {
-            for(size_t j = 0; j < Ny; j++) {
-                for(size_t k = 1; k < Nz; k++) {
-                    zSwap[i][j][k] = uz[i][j][k];
-                }
+    }
+    for(size_t i = 0; i < Nx; i++) {
+        for(size_t j = 0; j < Ny; j++) {
+            for(size_t k = 1; k < Nz; k++) {
+                uz[i][j][k] = zSwap[i][j][k];
             }
         }
+    }
     }
 }
 
